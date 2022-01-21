5ca7b00b5289a6a87d63ae0bcf15f3ec6104a18c
gi_rahulak <rahul.ak@giconsulting.com>
2022-01-21 21:30:36 +0530

    single book page

--- books/urls.py
+++ books/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name="book.all"),
+    path('<int:id>', views.show, name="book.show")
 ]
  
--- books/views.py
+++ books/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
 # from django.http import HttpResponse
+import json
+
+booksData = open(
+    '/home/rahul/Projects/python/bookstore/books.json'
+).read()
+
+data = json.loads(booksData)
 
 # Create your views here.
 def index(request):
-    context = {'name' : 'Rahul'}
+    context = {'books' : data}
     return render(request, 'books/index.html', context)
+
+
+def show(request, id):
+    book = None
+    for singleBook in data:
+        if (singleBook['id'] == id):
+            book = singleBook
+    context = {'book' : book}
+    return render(request, 'books/show.html', context)
